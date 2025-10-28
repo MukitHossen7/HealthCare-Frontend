aa076903372437cf1a2b2ca95e2cfa3c1f353fd0
MukitHossen7 <hossenmukit7@gmail.com>
2025-10-28 08:50:27 +0600

    first commit

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>welcome to next.js - 16</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
