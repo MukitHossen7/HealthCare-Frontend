e68dc4495093c8e2ed003ce7733416849f0e5226
MukitHossen7 <hossenmukit7@gmail.com>
2025-10-28 14:59:09 +0600

    desing home page

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,17 @@
 import { HeroSection } from "@/components/modules/Home/HeroSection";
+import Specialties from "@/components/modules/Home/Specialties";
+import Steps from "@/components/modules/Home/Steps";
+import Testimonials from "@/components/modules/Home/Testimonials";
+import TopRatedDoctors from "@/components/modules/Home/TopRatedDoctors";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <Specialties />
+      <TopRatedDoctors />
+      <Steps />
+      <Testimonials />
     </>
   );
 }
