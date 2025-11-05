f3f1b4e1c9e6229442ae3dc5d3f764c27d40b9dd
MukitHossen7 <hossenmukit7@gmail.com>
2025-11-05 21:10:42 +0600

    create forget and reset password page

---
+++ src/app/(auth)/forget-password/page.tsx
@@ -0,0 +1,11 @@
+const ForgetPasswordPage = () => {
+  return (
+    <>
+      <h1 className="text-4xl text-center py-10">
+        This is Forget Password Page
+      </h1>
+    </>
+  );
+};
+
+export default ForgetPasswordPage;
---
+++ src/app/(auth)/reset-password/page.tsx
@@ -0,0 +1,9 @@
+const ResetPasswordPage = () => {
+  return (
+    <>
+      <h1 className="text-4xl text-center">This is Reset Password Page</h1>
+    </>
+  );
+};
+
+export default ResetPasswordPage;
